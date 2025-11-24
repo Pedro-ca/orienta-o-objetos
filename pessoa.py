db163b361962c0ea91c9bb786b2ca12e1482a1e0
Pedro-ca <pedro.decastrom@gmail.com>
2025-11-24 16:58:42 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,14 +1,20 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = int):
+    def __init__(self,*filhos, nome = None, idade = 16):
+        self.idade = idade
         self.nome = nome   
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'ola {id(self)}'
     
 if __name__ == '__main__':
-    p = Pessoa('Pedro')
+    Vito = Pessoa(nome = 'Vito')
+    Pedro = Pessoa(Vito,nome = 'Pedro')
     print(Pessoa.cumprimentar)
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Victor'
-    print(p.nome)
+    print(Pedro.cumprimentar())
+    print(Pedro.nome)
+    for filho in  Pedro.filhos:
+        print(filho.nome)
+    Pedro.sobrenome = 'de Castro'
+    print(Pedro.__dict__)
+    print(Vito.__dict__)
